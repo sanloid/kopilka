8662e295e8c5ed215b0d20356e1632323b7bd641
Vasilchenko Alexander <sanloid2@gmail.com>
2022-07-26 12:31:32 +0600

    so

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { CellpropType } from '../types/CellType'
 
 export default function Cell(props: CellpropType) {
+    function cellFunc(){
+        alert("hi");
+    }
     return (
-        <div className={props.done ? "cellDone" : "cellNotDone"} >
+        <div className={props.done ? "cellNotDone" : "cellNotInPlan"} onClick={cellFunc}>
             <div className='cellDate'>
                 {props.data.date.toLocaleString(undefined, { minimumIntegerDigits: 2 })}.{(props.data.month + 1).toLocaleString(undefined, { minimumIntegerDigits: 2 })}.{props.data.year}
             </div>
